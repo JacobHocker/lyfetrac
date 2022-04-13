0bbcea0beb39a27055206dd4387b87d98a4dbc7d
Jacob Hocker <jacobdouglas06@gmail.com>
2022-04-12 19:06:19 -0500

    movie api methods and routes setup

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,22 @@
 require 'dotenv'
 class MoviesController < ApplicationController
 
+    # ! SINGLE MOVIE PARAMS = MOVIE ID # & API KEY
     def get_single_movie
         url = "https://api.themoviedb.org/3/movie/#{params[:movie_id]}?api_key=#{params[:api_key]}"
         response = HTTParty.get(url)
         render json: response
+    end
+
+    # ! SEARCH MOVIE PARAMS = SEARCH TERM & API KEY
+    def get_movie_search
+        url = "https://api.themoviedb.org/3/search/movie?api_key=#{params[:api_key]}&language=en-US&query=#{params[:search_term]}&page=1"
+        response = HTTParty.get(url)
+        render json: response
+    end
+
+    # ! TOP RATED MOVIES PARAMS = API KEY & PAGE NUMBER
+    def get_top_rated_movies
+        url = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{params[:api_key]}&language=en-US&page=#{params[:page_number]}"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   get '/single/:movie_id/:api_key', to: "movies#get_single_movie"
   
+  get '/movie-search/:api_key/:search_term', to: "movies#get_movie_search"
+
+  get '/top-rated/:api_key/:page_number', to: "movies#get_top_rated_movies"
   resources :users
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
