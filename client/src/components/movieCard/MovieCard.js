2459bbe653fa826b8f249d0a2a19f3ea6710a9af
JacobHocker <jacobdouglas06@gmail.com>
2022-04-18 14:37:03 -0500

    added responsive features to the movie page | added pagination to the movie page

--- client/src/components/movieCard/MovieCard.js
+++ client/src/components/movieCard/MovieCard.js
@@ -3,15 +3,37 @@ import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 import './MovieCard.scss';
 
-function MovieCard({ title, posterPath, id }) {
+function MovieCard({ title, posterPath, id, rating }) {
+    const movieTitles = () => {
+        if(title.length <= 27) {
+
+        }
+    }
+
+    const movieRatings = () => {
+        if(rating <= 1.0) {
+            return <Card.Footer className="terrible-rated-movie">{rating}</Card.Footer>
+        } if (rating >= 1.0 && rating <= 3.0) {
+            return <Card.Footer className="poor-rated-movie">{rating}</Card.Footer>
+        } if (rating >= 3.0 && rating <= 6.0) {
+            return <Card.Footer className="fair-rated-movie">{rating}</Card.Footer>
+        } if (rating >= 6.0 && rating <= 9.0) {
+            return <Card.Footer className="good-rated-movie">{rating}</Card.Footer>
+        } else {
+            return <Card.Footer className="great-rated-movie">{rating}</Card.Footer>
+        }
+    }
+    
     return(
         <div className="card-container">
             <Link to={`/movies/${id}`}>
                 <Card  className='movie-card'>
                     <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w185${posterPath}`}  className='movie-card-poster'/>
                     <Card.Body>
-                        <Card.Title className='movie-card-title'>{title}</Card.Title>
+                        <Card.Title className='movie-card-title'>{title}</Card.Title> 
+                        
                     </Card.Body>
+                    {movieRatings()}
                 </Card>
             </Link>
         </div>
--- client/src/components/moviesContainer/MoviesContainer.js
+++ client/src/components/moviesContainer/MoviesContainer.js
@@ -1,31 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { CardGroup, Container, Row, Col } from 'react-bootstrap';
+import { CardGroup, Container, Row, Col, Pagination } from 'react-bootstrap';
 import MovieCard from "../movieCard/MovieCard";
 import './MoviesContainer.scss';
 
 function MoviesContainer() {
     const [topRatedMovies, setTopRatedMovies] = useState([]);
-    const [activePage, setActivePage] = useState(1)
-    const [activeCategory, setActiveCategory] = useState("top-rated")
+    const [activePage, setActivePage] = useState(1);
+    const [activeCategory, setActiveCategory] = useState("popular");
+    const [pageSet, setPageSet] = useState("page-set-one");
+    
     
     const categories = [
-        {id: 0, value: "top-rated", title: "Top Rated"},
-        {id: 1, value: "popular", title: "Popular"},
+        {id: 0, value: "popular", title: "Popular"},
+        {id: 1, value: "top-rated", title: "Top Rated"},
         {id: 2, value: "now-playing", title: "Now Playing"},
         {id: 3, value: "upcoming", title: "Upcoming"}
     ]
-    const pages = [
+    const pagesOne = [
         {id: 1, value: 1},
         {id: 2, value: 2},
         {id: 3, value: 3},
         {id: 4, value: 4},
         {id: 5, value: 5},
+    ]
+    const pagesTwo = [
         {id: 6, value: 6},
         {id: 7, value: 7},
         {id: 8, value: 8},
         {id: 9, value: 9},
         {id: 10, value: 10},
     ]
+
+    
     const apiKey = '4a571a843827a09096250c11596c470d'
     const pageNumber = activePage
 
@@ -40,15 +46,8 @@ function MoviesContainer() {
     }, [pageNumber, activeCategory]);
     
     const results = topRatedMovies.results
+    
 
-    const pageListDisplay = pages.map((page) => (
-        <li className={activePage === page.value ? "active-page" : "inactive-page"}
-            key={page.id}
-            onClick={() => setActivePage(page.value)}
-            >
-            {page.value}
-            </li>
-    ))
     
     
     return(
@@ -80,11 +79,29 @@ function MoviesContainer() {
                 </Row>
                 <Row>
                     <Col>
-                        <div className="page-select-container">
-                            <h2>Pages:</h2>
-                            <ul className="page-list">
-                                {pageListDisplay}
-                            </ul>
+                        <div className="pagination-container">
+                            <Pagination >
+                                <Pagination.Prev onClick={() => setPageSet('page-set-one')}/>
+                                    {pageSet === 'page-set-one' ? 
+                                    pagesOne.map((pone) => (
+                                        <Pagination.Item 
+                                        key={pone.id}
+                                        active={activePage === pone.value}
+                                        onClick={() => setActivePage(pone.value)}>
+                                            {pone.value}
+                                        </Pagination.Item>
+                                        )) :
+                                    pagesTwo.map((ptwo) => (
+                                        <Pagination.Item 
+                                        key={ptwo.id}
+                                        active={activePage === ptwo.value}
+                                        onClick={() => setActivePage(ptwo.value)}>
+                                            {ptwo.value}
+                                        </Pagination.Item>
+                                        ))
+                                    }
+                                <Pagination.Next onClick={() => setPageSet('page-set-two')}/>
+                            </Pagination>
                         </div>
                     </Col>
                 </Row>
@@ -95,22 +112,39 @@ function MoviesContainer() {
                             title={movie.title}
                             posterPath={movie.poster_path}
                             id={movie.id}
+                            rating={movie.vote_average}
                         />
                     )) }
                 </CardGroup>
                 <Row>
                     <Col>
-                        <div className="page-select-container">
-                            <h2>Pages:</h2>
-                            <ul className="page-list">
-                                {pageListDisplay}
-                            </ul>
+                    <div className="pagination-container">
+                            <Pagination >
+                                <Pagination.Prev onClick={() => setPageSet('page-set-one')}/>
+                                    {pageSet === 'page-set-one' ? 
+                                    pagesOne.map((pone) => (
+                                        <Pagination.Item 
+                                        key={pone.id}
+                                        active={activePage === pone.value}
+                                        onClick={() => setActivePage(pone.value)}>
+                                            {pone.value}
+                                        </Pagination.Item>
+                                        )) :
+                                    pagesTwo.map((ptwo) => (
+                                        <Pagination.Item 
+                                        key={ptwo.id}
+                                        active={activePage === ptwo.value}
+                                        onClick={() => setActivePage(ptwo.value)}>
+                                            {ptwo.value}
+                                        </Pagination.Item>
+                                        ))
+                                    }
+                                <Pagination.Next onClick={() => setPageSet('page-set-two')}/>
+                            </Pagination>
                         </div>
                     </Col>
                 </Row>
             </Container>
-            
-            
         </div>
     );
 };
