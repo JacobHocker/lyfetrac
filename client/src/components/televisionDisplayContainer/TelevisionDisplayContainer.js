5b2a8ccfdbec17d539d14ac11f6ffd4d8e265507
JacobHocker <jacobdouglas06@gmail.com>
2022-05-02 10:33:48 -0500

    set up tv back drop and tv poster in the tv display header container

--- client/src/components/televisionDisplayContainer/TelevisionDisplayContainer.js
+++ client/src/components/televisionDisplayContainer/TelevisionDisplayContainer.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import TelevisionDisplayHeader from '../televisionDisplayHeader/TelevisionDisplayHeader';
 import './TelevisionDisplayContainer.scss';
 
 function TelevisionDisplayContainer() {
+    const [tvShow, setTvShow] = useState({});
+
+
+    const apiKey = '4a571a843827a09096250c11596c470d';
+    const {id} = useParams()
+    const pageNumber = 1
+    //TV SHOW
+    useEffect(() => {
+        fetch(`/single-tv/${id}/${apiKey}`)
+        .then((r) => r.json())
+        .then((tvShow) => { setTvShow(tvShow);
+        });
+    }, [id]);
+
+    
     return(
-        <div className='television-display-container'>
-            <h1>TV Display Container</h1>
+        <div className='tv-display-container'>
+            {tvShow.id && <TelevisionDisplayHeader tvShow={tvShow} />}
         </div>
     );
 }
--- client/src/components/televisionDisplayHeader/TelevisionDisplayHeader.js
+++ client/src/components/televisionDisplayHeader/TelevisionDisplayHeader.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import defaultMovie from '../../assets/images/defaultmovie.jpg';
 import './TelevisionDisplayHeader.scss';
 
-function TelevisionDisplayHeader() {
+function TelevisionDisplayHeader({ tvShow }) {
+
+    console.log(tvShow)
     return(
-        <div className='television-display-header-container'>
-            <h1>TV Display Header</h1>
+        <div className='tv-display-header-container'>
+            <Container>
+                <div className="tv-display-header" 
+                    style={{ 
+                        backgroundImage: `url(https://image.tmdb.org/t/p/w500${tvShow.backdrop_path})`,
+                        backgroundPosition: 'center top',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat',
+                        width: "100%",
+                        boxShadow: "inset 0 0 0 1000px rgba(52.5, 73.5, 115.5,.85)"
+                    }}>
+                    <Row>
+                        <Col>
+                            <div className="tv-poster-display-container">
+                                <img src={tvShow.poster_path === null ? defaultMovie : `https://image.tmdb.org/t/p/w300${tvShow.poster_path}`} alt={tvShow.name} className="tv-display-poster" />
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className='tv-display-title-container'>
+                                <div className="tv-display-title">
+                                    <h1>{tvShow.name}</h1> 
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+            </Container>
         </div>
     );
 }
