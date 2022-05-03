365cceae7ff1d491dd50838772297505b6de5824
JacobHocker <jacobdouglas06@gmail.com>
2022-05-03 11:29:09 -0500

    reused the styles of the movie reviews for the tv show display page and the tv show review cards

--- client/src/components/televisionDisplayContainer/TelevisionDisplayContainer.js
+++ client/src/components/televisionDisplayContainer/TelevisionDisplayContainer.js
@@ -5,12 +5,15 @@ import TelevisionDisplayCrew from '../televisionDisplayCrew/TelevisionDisplayCre
 import TelevisionDisplayHeader from '../televisionDisplayHeader/TelevisionDisplayHeader';
 import TelevisionDisplayInfo from '../televisionDisplayInfo/TelevisionDisplayInfo';
 import TelevisionDisplayRecommendation from '../televisionDisplayRecommendation/TelevisionDisplayRecommendation';
+import TelevisionDisplayReviewContainer from '../televisionDisplayReviewContainer/TelevisionDisplayReviewContainer';
 import './TelevisionDisplayContainer.scss';
 
 function TelevisionDisplayContainer() {
     const [tvShow, setTvShow] = useState({});
     const [tvCredits, setTvCredits] = useState([]);
     const [tvRecommend, setTvRecommend] = useState([]);
+    const [tvReviews, setTvReviews] = useState([]);
+
 
     const apiKey = '4a571a843827a09096250c11596c470d';
     const {id} = useParams()
@@ -36,6 +39,14 @@ function TelevisionDisplayContainer() {
         .then((tvRecommend) => { setTvRecommend(tvRecommend);
         });
     }, [id]);
+    //TV SHOW REVIEWS
+    useEffect(() => {
+        fetch(`/tv-reviews/${id}/${apiKey}`)
+        .then((r) => r.json())
+        .then((tvReviews) => { setTvReviews(tvReviews);
+        });
+    }, [id]);
+    
 
     
     return(
@@ -43,6 +54,7 @@ function TelevisionDisplayContainer() {
             {tvShow.id && <TelevisionDisplayHeader tvShow={tvShow} />}
             {tvCredits.cast && <TelevisionDisplayCast cast={tvCredits.cast} />}
             {tvShow.id && <TelevisionDisplayInfo tvShow={tvShow} />}
+            {tvReviews.results && <TelevisionDisplayReviewContainer reviews={tvReviews.results} />}
             {tvCredits.crew && <TelevisionDisplayCrew crew={tvCredits.crew} />}
             {tvRecommend.results && <TelevisionDisplayRecommendation shows={tvRecommend.results} />}
         </div>
