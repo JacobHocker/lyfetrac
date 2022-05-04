86beec7d497ffaf8c3be9575ec47c73e64a21325
JacobHocker <jacobdouglas06@gmail.com>
2022-05-04 11:55:15 -0500

    completed styling and making responsive the seasons container to select which season to view

--- client/src/components/seasonsContainer/SeasonsContainer.js
+++ client/src/components/seasonsContainer/SeasonsContainer.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import SeasonsContainerSections from "../seasonsContainerSections/SeasonsContainerSections";
+//import { Link } from "react-router-dom";
 import './SeasonsContainer.scss';
 
 function SeasonsContainer() {
@@ -18,10 +20,28 @@ function SeasonsContainer() {
     }, [id]);
     
 
-    console.log(tvShow)
+    
     return(
         <div className="seasons-container">
-            
+            {tvShow.id && 
+            <Container style={{ 
+                        backgroundImage: `url(https://image.tmdb.org/t/p/w500${tvShow.backdrop_path})`,
+                        backgroundPosition: 'center top',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat',
+                        width: "100%",
+                        boxShadow: "inset 0 0 0 1000px rgba(52.5, 73.5, 115.5,.85)"
+                    }}>
+                <Row>
+                    
+                    <Col>
+                        <div className="seasons-container-title">
+                            <h1>{tvShow.name}'s Seasons</h1>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>}
+            {tvShow.seasons && <SeasonsContainerSections seasons={tvShow.seasons} id={tvShow.id} />}
         </div>
     );
 }
---
+++ client/src/components/seasonsContainerSections/SeasonsContainerSections.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import defaultMovie from '../../assets/images/defaultmovie.jpg';
+import './SeasonsContainerSections.scss';
+
+
+function SeasonsContainerSections({ seasons, id }) {
+    
+    console.log(seasons)
+
+    const seasonSectionMap = seasons.map((season) => (
+        <Row key={season.id} className='season-section-row'>
+            <Col>
+                <div className="season-section-poster-container">
+                    <img src={season.poster_path === null ? defaultMovie : `https://image.tmdb.org/t/p/w300${season.poster_path}`} alt={season.name} className="season-section-poster" />
+                    <div className="season-section-season-name">
+                        <h1>{season.name}</h1>
+                    </div>
+                </div>
+            </Col>
+            <Col>
+                <div className="season-section-name-container">
+                    <div className="season-section-season-aired">
+                        <h4>{season.air_date}</h4>
+                    </div>
+                    <div className="season-section-season-episodes">
+                        <h4>Episodes: {season.episode_count}</h4>
+                    </div>
+                </div>
+            </Col>
+            <Col>    
+                <div className="season-section-season-link">
+                    <Link to={`/tv/${id}/season/${season.id}`}>
+                        <button className="season-section-link-button">See {season.name}</button>
+                    </Link>
+                </div>
+            </Col>
+        </Row>
+    ))
+    return(
+        <div className="seasons-section-container">
+            <Container>
+                {seasonSectionMap}
+            </Container>
+        </div>
+    );
+}
+
+export default SeasonsContainerSections;
